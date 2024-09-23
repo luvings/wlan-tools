17e2e37ae5d20828ad9b2ff82603e4de2f16f009
huang <luvings@live.cn>
2024-09-23 15:29:18 +0800

    add libnl-3.0

---
+++ libnl-3.0/doc/src/examples/nla_for_each_attr.c
@@ -0,0 +1,12 @@
+#include <netlink/attr.h>
+
+struct nalttr *nla;
+int rem;
+
+nla_for_each_attr(nla, attrstream, streamlen, rem)
+{
+	/* validate & parse attribute */
+}
+
+if (rem > 0)
+	/* unparsed attribute data */
